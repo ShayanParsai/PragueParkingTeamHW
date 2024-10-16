b4beff3e29b12fa8deb79126aab07a9efbd4b5b3
Basel El Omar <elomarbasel@gmail.com>
2024-10-16 11:04:37 +0200

    staged

--- HWPragueParkingV1/obj/Debug/net8.0/HWPragueParkingV1.AssemblyInfo.cs
+++ HWPragueParkingV1/obj/Debug/net8.0/HWPragueParkingV1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HWPragueParkingV1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0457b3553f03c15a6662cf8c72a8edcbc0f8b00b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0741f9786326039c0b2df7cda6ff01aaba31c30")]
 [assembly: System.Reflection.AssemblyProductAttribute("HWPragueParkingV1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HWPragueParkingV1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
